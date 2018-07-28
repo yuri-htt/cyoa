a13124a4d9c1361d4ba00a46543a809964f8f497
Yuri <yuri.tayama@linksports.co.jp>
2018-07-29 01:56:28 +0900

    テンプレートにスタイルを追加

--- story.go
+++ story.go
@@ -22,21 +22,64 @@ var defaultHandlerTmpl = `
         <meta charset="utf-8">
         <title>Choose Your Own Adventure</title>
     </head>
-    <body>
-        <h1>{{.Title}}</h1>
-        {{range .Paragraphs}}
-        <p>{{.}}</p>
-        {{end}}
-        <ul>
-        {{range .Options}}
-            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-        {{end}}
-        </ul>
+	<body>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+			{{range .Paragraphs}}
+			<p>{{.}}</p>
+			{{end}}
+			<ul>
+			{{range .Options}}
+				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+			</ul>
+		</section>
+		<style>
+		body {
+			font-family: helvetica, arial;
+		  }
+		  h1 {
+			text-align:center;
+			position:relative;
+		  }
+		  .page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FFFCF6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777;
+		  }
+		  ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		  }
+		  li {
+			padding-top: 10px;
+		  }
+		  a,
+		  a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		  }
+		  a:active,
+		  a:hover {
+			color: #7792a2;
+		  }
+		  p {
+			text-indent: 1em;
+		  }
+		</style>
     </body>
 </html>
 `
 
 // 構造体のHandlerはhttpパッケージで定義されており、ServeHTTP(ResponseWriter, *Rewust)で構成される
+// type Handler:
 func NewHandler(s Story) http.Handler {
 	return handler{s}
 }
@@ -45,6 +88,7 @@ type handler struct {
 	s Story
 }
 
+// type handlerFunc
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
