bbe308f453082223ac644dd79e2e1a8472567b4f
Yuri <yuri.tayama@linksports.co.jp>
2018-07-28 23:22:40 +0900

    デコードしたJSONをヘッダーに渡してport3000でテンプレートを起動

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/yuri-swift/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s. \n", *filename)
@@ -19,11 +21,12 @@ func main() {
 		panic(err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
-		panic(nil)
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("%;v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
