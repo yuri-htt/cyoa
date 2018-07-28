436e9472b37426d7298b3ec81fa0b3f3f10e80b6
Yuri <yuri.tayama@linksports.co.jp>
2018-07-28 23:14:10 +0900

    JSONのデコード処理

--- story.go
+++ story.go
@@ -1,5 +1,23 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+// io.Reader: バイト列を読むためのReadメソッドを提供する
+func JsonStory(r io.Reader) (Story, error) {
+	// デコード: 別ファイルを読み取ってjsonに変換し直す
+	d := json.NewDecoder(r)
+	var story Story
+	// 構造体Chapterを要素とする配列の構造体のポインタ(アドレスのみ)を渡してデコード
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
 type Story map[string]Chapter
 
 type Chapter struct {
